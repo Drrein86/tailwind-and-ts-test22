4a20a70b0b1fb0ce45193e66ebd6d75623b03cf6
Drrein86 <elior2280@gmail.com>
2022-11-08 18:27:52 +0200

    fu

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,26 @@ import { Country } from "./Components/interfaces";
 function App() {
   return (
     <div className={"App"}>
-      <div className={"card"}>sdsd</div>
-      <p>sdasdasdas sssasdassssdasd</p>
-      <Person
-        name="big bag"
-        email="ssss@sadas.com"
-        age={24}
-        isMarried={true}
-        friends={["jake", "ido", "moshe"]}
-        country={Country.island}
-      />
+      <div className={"card"}></div>
+      <img src="images\user.png" />
+      <div className="person">
+        <Person
+          name="big bag"
+          email="ssss@sadas.com"
+          age={24}
+          isMarried={true}
+          friends={["jake", "ido", "moshe"]}
+          country={Country.island}
+        />
+      </div>
+      <div className={"buttons"}>
+        <a href="#" className={"button AFbutton"}>
+          Add friddend
+        </a>
+        <a href="#" className={"button MSGbutton"}>
+          Message
+        </a>
+      </div>
     </div>
   );
 }
